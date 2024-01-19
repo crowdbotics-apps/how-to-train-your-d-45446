d7662c1bb75de1e9273ec63100819205b9a1fecd
Crowdbotics <team@crowdbotics.com>
2024-01-19 14:37:18 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "how-to-train-your-d-45446",
+  url: "https://how-to-train-your-d-45446.botics.co",
+  api: "https://how-to-train-your-d-45446.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
